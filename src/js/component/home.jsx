6786c5fbef5a06e49c909ffe3af2e0b1d5450496
amalia1501 <amibottini@hotmail.com>
2023-03-30 02:56:00 +0000

    LandPage

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import NavBar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<>
+
+			<NavBar/>
+			<div className="container">
+			<Jumbotron/>
+			<div className="container-fluid d-flex">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+		
+			</div>
+			<Footer/>
+			</>
+		
 	);
 };
 
